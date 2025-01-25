f590d5e5bdb282a15dd1ef431499ded0de6e7764
igfernandes <fernandes7.1@outlook.com>
2025-01-25 18:48:43 -0300

    fix: fixing import of InstanceDatabase with name incorrect

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { InstanceDatabase } from "./database/InstaceDatabase";
+import { InstanceDatabase } from "@database/instanceDatabase";
 
 const instanceDb = new InstanceDatabase();
 instanceDb.getInstance().ddb.local();
